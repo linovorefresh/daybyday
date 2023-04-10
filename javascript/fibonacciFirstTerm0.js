3cb1ea3fb140380d32b1b994408ebad278d606a1
linovorefresh <linolp@gmail.com>
2023-04-10 13:11:46 -0700

    fibonacci function with 0 as 1st term

---
+++ javascript/fibonacciFirstTerm0.js
@@ -0,0 +1,12 @@
+let fibonacci = (n) => {
+    let array = [0,1];
+    if(n<=2) return n-1;
+    for(let i = 2; i < n; i++){
+        array.push(array[array.length - 2] + array[array.length - 1]);
+    }
+    return array[n-1];
+};
+
+// Above is a fibonacci arrow function with the parameter being the nth entry in the sequence. 
+// ie. fibonacci(8) returns 13 and fibonacci(10) returns 34
+console.log(fibonacci(4));
